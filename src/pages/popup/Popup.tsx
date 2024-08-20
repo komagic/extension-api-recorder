3d25b37fa9f9cb735405de2fcb05679769fa50bd
komagic <910367438@qq.com>
2024-08-20 17:54:21 +0800

    feat(popover): add clear

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,13 +1,35 @@
 'use client';
+import { ClearOutlined ,CaretRightOutlined, VideoCameraFilled } from '@ant-design/icons';
 import '@pages/popup/Popup.css';
 // import useStorage from '@src/shared/hooks/useStorage';
 // import exampleThemeStorage from '@src/shared/storages/exampleThemeStorage';
 import withErrorBoundary from '@src/shared/hoc/withErrorBoundary';
 import withSuspense from '@src/shared/hoc/withSuspense';
-import { Card, ConfigProvider, theme } from 'antd';
+import { Button, Card, ConfigProvider, Popconfirm, Popover, theme } from 'antd';
+import { useCallback } from 'react';
+import AppStore from '../content/ui/Context/AppStore';
+import packagejson from '@root/package.json';
 
 const Popup = () => {
   // const theme = useStorage(exampleThemeStorage);
+const handleOpen = useCallback(
+  () => {
+    
+  },
+  [],
+)
+
+const handleClear = () => {
+  AppStore.clear();
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    if (tabs.length > 0) {
+      const tabId = tabs[0].id;
+      chrome.tabs.sendMessage(tabId, { action: 'recorder:clearIndexedDB' });
+      chrome.tabs.reload(tabId);
+    }
+});
+}
+
 
   return (
     <ConfigProvider
@@ -19,11 +41,40 @@ const Popup = () => {
       <Card
         style={{
           borderRadius: 0,
+          
+        }}
+        styles={{
+          body:{
+          paddingTop: 0,
+
+          }
         }}
         className="dar h-full w-full border-radius-0"
-        title="API Recorder">
-        {/* <Button onClick={handleOpen}>启用</Button>
-        <Button onClick={handleDisable}>禁用</Button> */}
+        title={ <span
+          style={{
+            fontSize: 16,
+            lineHeight: '1rem',
+          }}>
+          <span style={{ marginRight: 8 }}>
+            <VideoCameraFilled />
+          </span>
+          API Recorder <span style={{
+            fontSize: 12,
+            marginLeft: 8,
+            color:'rgba(253,253,253,0.2)',
+            fontStyle:'italic'
+          }}>{packagejson?.version}</span>
+        </span>}>
+          <p>轻量化、快捷HTTP拦截器</p>
+     <div className='grid grid-cols-2 gap-2'>
+         <Button size='small' type='primary' icon={<CaretRightOutlined />}  onClick={handleOpen}>全局启用</Button>
+        
+        <Popconfirm cancelText={
+          '取消'
+        } okText='确定' title="确定清空所有数据缓存吗？"  onConfirm={handleClear}>
+        <Button size='small' icon={<ClearOutlined />} danger >清空缓存</Button>
+        </Popconfirm>
+     </div>
       </Card>
     </ConfigProvider>
   );
